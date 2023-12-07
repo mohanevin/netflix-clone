495d3fa7dddb13449fd8a1bfa2fc830dc94bd604
Mohan I <96979919+mohanevin@users.noreply.github.com>
2023-12-07 13:12:48 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOTk1ZDdhMjJkYTkyMzFiOTljOTk0ZmQ0YWU2YjRkNyIsInN1YiI6IjY1NDhmNTNiNDM0OTRmMDEwMWM5MTdjYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bN4XP-abwbQg_i_ylJEmZ9KLl-dsyLd9wF4M1PJr-o8'
+      Authorization: 'Bearer'+ process.env.REACT_APP_TMDB_KEY
     }
   };
 
